bea175d9f16860d38d01cf5cd448808e63b7b541
AhnafMuttaki <ahnafx88@gmail.com>
2022-09-23 20:15:04 +0600

    try latest php and mysql

--- install_moodle_in_amazon_linux.sh
+++ install_moodle_in_amazon_linux.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 sudo yum update -y
-sudo amazon-linux-extras install nginx1 php7.4 -y
+sudo amazon-linux-extras install nginx1 php8.0 -y
 sudo yum clean metadata
-sudo yum install git mariadb-server php-{pear,cgi,common,curl,mbstring,gd,mysqlnd,gettext,bcmath,json,xml,fpm,intl,zip} -y
+sudo yum install git php-{pear,cgi,common,curl,mbstring,gd,mysqli,mysqlnd,gettext,bcmath,json,xml,fpm,intl,zip} -y
 # Back up existing config
 sudo cp -R /etc/nginx /etc/nginx-backup
 sudo chmod -R 777 /var/log
